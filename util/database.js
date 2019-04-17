aed201e85edd141879461c18f8b35a53e25d3cb8
gr33ngr33n <actfun07@gmail.com>
2019-04-18 00:30:28 +0800

    refactor DB connection to cater to multiple calls

--- util/database.js
+++ util/database.js
@@ -2,13 +2,35 @@ const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = callback => {
-    MongoClient.connect('mongodb+srv://actfun07_user1:actfun07_pwuser1@cluster0-j3s72.mongodb.net/test?retryWrites=true')
+    MongoClient.connect('mongodb+srv://actfun07_user1:actfun07_pwuser1@cluster0-j3s72.mongodb.net/shop?retryWrites=true')
     .then(client => {
         console.log('connected!');
-        callback(client);
+        _db = client.db();  // create db automatically if not available, it will create it on the fly
+
+        /* 
+        example above:
+        - in the URL, the db "shop" will be created ... client.db()
+        - if client.db('dummy'), the db "dummy" will be created instead of "shop"
+        */
+
+        callback();
     })
-    .catch(err => {console.log(err)});
+    .catch(err => {
+        console.log(err);
+        throw err;
+    });
 };
 
-module.exports = mongoConnect;
+const getDB = () => {
+    if (_db) {
+        return _db;
+    }
+    throw "No DB Found!"
+}
+
+
+exports.mongoConnect = mongoConnect;
+exports.getDB = getDB;
