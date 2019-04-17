db7fa9a20fa6f91b7b6d7a2b9ba2c4220643ca37
gr33ngr33n <actfun07@gmail.com>
2019-04-18 00:31:56 +0800

    refactor Product model

--- models/product.js
+++ models/product.js
@@ -1,27 +1,18 @@
-const Sequelize = require('sequelize');
+const getDB = require('../util/database').getDB;
 
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('copy_sqlz_product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
-});
+
+  save() {
+    const db = getDB();
+    return db.collection('products').insertOne(this);
+  }
+}
+
 
 module.exports = Product;
