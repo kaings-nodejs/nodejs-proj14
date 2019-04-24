9d289207e4169d917dce42da73bbca6652f8c74b
gr33ngr33n <actfun07@gmail.com>
2019-04-24 14:21:12 +0800

    remove comment

--- controllers/shop.js
+++ controllers/shop.js
@@ -28,19 +28,6 @@ exports.getProduct = (req, res, next) => {
   .catch(err => {
     console.log(err);
   });
-
-// the following approach also works the same
-/* 
-  Product.findAll({where: {id: prodId}})
-  .then(products => {
-    res.render('shop/product-detail', {
-      product: products[0],
-      pageTitle: products[0].title,
-      path: '/products'
-    });
-  })
-  .catch(err => {console.log(err)});
-   */
 };
 
 exports.getIndex = (req, res, next) => {
