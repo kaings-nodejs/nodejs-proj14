bec1e51ec4081bf1d35b15c57c88a564d9d34aaf
gr33ngr33n <actfun07@gmail.com>
2019-05-08 00:08:59 +0800

    connect to mongoDB server with Mongoose

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
 const User = require('./models/user');
 
 const app = express();
@@ -19,20 +19,23 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use((req, res, next) => {   // this will be run when there is any incoming request. it is put on top of all routes. All incoming request will trigger this middleware
-    User.findById('5cbeddce7f959c02b4ba222c')
-    .then(user => {
-        console.log('user..... ', user);
-        req.user = new User(user.username, user.email, user._id, user.cart);    // store object "user" into "req.user" so that it can be called/used globally
-        next();     // move to the next middleware
-    })
-    .catch(err => {console.log(err)});
-});
+// app.use((req, res, next) => {   // this will be run when there is any incoming request. it is put on top of all routes. All incoming request will trigger this middleware
+//     User.findById('5cbeddce7f959c02b4ba222c')
+//     .then(user => {
+//         console.log('user..... ', user);
+//         req.user = new User(user.username, user.email, user._id, user.cart);    // store object "user" into "req.user" so that it can be called/used globally
+//         next();     // move to the next middleware
+//     })
+//     .catch(err => {console.log(err)});
+// });
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
+mongoose.connect('mongodb+srv://actfun07_user1:actfun07_pwuser1@cluster0-j3s72.mongodb.net/shop_mongoose?retryWrites=true')
+.then(result => {
+    console.log(result);
     app.listen(3000);
-});
+})
+.catch(err => {console.log(err)});
