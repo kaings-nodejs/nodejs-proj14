3d494211b912b1a6088227a1586c2d2f09bfcb3c
gr33ngr33n <actfun07@gmail.com>
2019-05-08 00:11:18 +0800

    save product data through Mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -14,7 +14,12 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(title, price, description, imageUrl);
+  const product = new Product({
+    title: title,
+    imageUrl: imageUrl,
+    price: price,
+    description: description
+  });
 
   product.save()
   .then(result => {
