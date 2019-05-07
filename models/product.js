f6c8f7fc2385150a150f0eddc2bca539acc9fce2
gr33ngr33n <actfun07@gmail.com>
2019-05-07 23:56:46 +0800

    init housekeeping

--- models/product.js
+++ models/product.js
@@ -1,48 +1,48 @@
-const mongodb = require('mongodb');
-const getDB = require('../util/database').getDB;
+// const mongodb = require('mongodb');
+// const getDB = require('../util/database').getDB;
 
-class Product {
-  constructor(title, price, description, imageUrl, id) {
-    this.title = title;
-    this.price = price;
-    this.description = description;
-    this.imageUrl = imageUrl;
-    this._id = (id)? new mongodb.ObjectId(id) : null;
-  }
+// class Product {
+//   constructor(title, price, description, imageUrl, id) {
+//     this.title = title;
+//     this.price = price;
+//     this.description = description;
+//     this.imageUrl = imageUrl;
+//     this._id = (id)? new mongodb.ObjectId(id) : null;
+//   }
 
-  save() {
-    const db = getDB();
-    let dbOp;
+//   save() {
+//     const db = getDB();
+//     let dbOp;
 
-    if (this._id) {
-      dbOp = db.collection('products').updateOne( {_id: this._id}, {$set: this} );
-    } else {
-      dbOp = db.collection('products').insertOne(this);
-    }
+//     if (this._id) {
+//       dbOp = db.collection('products').updateOne( {_id: this._id}, {$set: this} );
+//     } else {
+//       dbOp = db.collection('products').insertOne(this);
+//     }
 
-    return dbOp;
-  }
+//     return dbOp;
+//   }
 
-  static fetchAll() {
-    const db = getDB();
-    return db.collection('products')
-    .find()
-    .toArray(); // find() return cursor that points to documents. Therefore, toArray() is needed to return Promise. ref: https://docs.mongodb.com/manual/reference/method/cursor.toArray/index.html
-  }
+//   static fetchAll() {
+//     const db = getDB();
+//     return db.collection('products')
+//     .find()
+//     .toArray(); // find() return cursor that points to documents. Therefore, toArray() is needed to return Promise. ref: https://docs.mongodb.com/manual/reference/method/cursor.toArray/index.html
+//   }
 
-  static findById(prodId) {
-    const db = getDB();
-    return db.collection('products')
-    .find( {_id: new mongodb.ObjectId(prodId)} )
-    .next();
-  }
+//   static findById(prodId) {
+//     const db = getDB();
+//     return db.collection('products')
+//     .find( {_id: new mongodb.ObjectId(prodId)} )
+//     .next();
+//   }
 
-  static deleteById(prodId) {
-    const db = getDB();
-    return db.collection('products')
-    .deleteOne({_id: new mongodb.ObjectId(prodId)});
-  }
-}
+//   static deleteById(prodId) {
+//     const db = getDB();
+//     return db.collection('products')
+//     .deleteOne({_id: new mongodb.ObjectId(prodId)});
+//   }
+// }
 
 
-module.exports = Product;
+// module.exports = Product;
