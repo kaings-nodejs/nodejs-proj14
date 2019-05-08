6182c118225cef0cd40300a841e8bbe79e1eae30
gr33ngr33n <actfun07@gmail.com>
2019-05-08 16:10:53 +0800

    create user & set user into req.user property in order to be accessed globally

--- app.js
+++ app.js
@@ -19,15 +19,15 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// app.use((req, res, next) => {   // this will be run when there is any incoming request. it is put on top of all routes. All incoming request will trigger this middleware
-//     User.findById('5cbeddce7f959c02b4ba222c')
-//     .then(user => {
-//         console.log('user..... ', user);
-//         req.user = new User(user.username, user.email, user._id, user.cart);    // store object "user" into "req.user" so that it can be called/used globally
-//         next();     // move to the next middleware
-//     })
-//     .catch(err => {console.log(err)});
-// });
+app.use((req, res, next) => {   // this will be run when there is any incoming request. it is put on top of all routes. All incoming request will trigger this middleware
+    User.findById('5cd28dd8cf381111b99915c5')
+    .then(user => {
+        console.log('user..... ', user);
+        req.user = user;   // store mongoose object "user" into "req.user" so that it can be called/used globally
+        next();     // move to the next middleware
+    })
+    .catch(err => {console.log(err)});
+});
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -36,6 +36,21 @@ app.use(errorController.get404);
 mongoose.connect('mongodb+srv://actfun07_user1:actfun07_pwuser1@cluster0-j3s72.mongodb.net/shop_mongoose?retryWrites=true')
 .then(result => {
     console.log(result);
-    app.listen(3000);
+
+    User.findOne().then(currentUser => {
+        console.log('mongoose.connect_currentUser..... ', currentUser);
+
+        if (!currentUser) {
+            const user = new User({
+                username: 'Will',
+                email: 'will@abc.com',
+                cart: { items: [] }
+            });
+
+            user.save();
+        }
+
+        app.listen(3000);
+    });
 })
 .catch(err => {console.log(err)});
